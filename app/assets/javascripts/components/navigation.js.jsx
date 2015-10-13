a77560ab780976988a4d3af25738fd40f411cf0f
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-10-13 16:52:00 -0500

    nav react bar working

---
+++ app/assets/javascripts/components/navigation.js.jsx
@@ -0,0 +1,18 @@
+var Navigation = React.createClass({
+  render: function() {
+    return (
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li><a href='/home'>Home</a></li>
+              <li><a href='/login'>Login</a></li>
+              <li><a href='/logout'>Logout</a></li>
+              <li><a href='/cheeses'>Cheese list</a></li>
+              <li><a href='/users'>List of Users</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>);
+  }
+});
